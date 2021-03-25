c47fc46f7db280c0e84f11eea6335286560347d4
kanekowinc <kanekowinc@gmail.com>
2021-03-25 09:44:17 +0900

    fileの引数で定員人数を指定できるようにしました。

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from flask import Flask, render_template
 import json
+import argparse
+
 
 app = Flask(__name__)
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-l", "--limit", type=int, default=4,
+	help="Please enter the maximum number of people")
+args = vars(ap.parse_args())
+
 # 定員
-limit = 4
+limit = args["limit"]
 
 @app.route('/')
 def get():
     try:
-        with open('tmp/persons.json') as f:
+        # with open('tmp/persons.json') as f:
+        with open('persons.json') as f:
             load = json.load(f)
 
             count = load['count']
             if count >= limit:
                 return render_template('danger.html', count=count ,limit=limit)
-            elif count >= (limit//2):
+            elif count >= (limit/2):
                 return render_template('caution.html', count=count ,limit=limit)
             else:
                 return render_template('safety.html', count=count ,limit=limit)
